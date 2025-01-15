262ddaa45a4f83ba9dcee943307e445f05be9a45
ParasRajBisht011 <parasrajbisht011@gmail.com>
2025-01-15 18:05:28 +0530

    selection sort method

---
+++ codes/selection_sort.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int selectionSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int min = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+        }
+        
+        int temp = arr[min];
+        arr[min] = arr[i];
+        arr[i] = temp;
+    }
+}
+
+int main() {
+    int arr[] = {3, 2, 9, 1, 5, 6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    selectionSort(arr, n);
+
+    printf("Sorted array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
